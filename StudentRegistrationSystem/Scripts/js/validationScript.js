d34088533638275fae775b4eb3fc0be45a592d79
suh07 <tsuhail07@gmail.com>
2022-11-03 09:11:53 +0400

    Update 6

--- StudentRegistrationSystem/Scripts/js/validationScript.js
+++ StudentRegistrationSystem/Scripts/js/validationScript.js
@@ -1,18 +1,13 @@
 ﻿function EmailValidation(element) {
     var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (element.value.length == 0) {
-        //errorMessages.push("Email must be specified");
         toastr.error("Email must be specified");
     }
     else if (element.value.length < 8 || element.value.length > 50) {
-        //.push("Email must be between 8 to 50 characters");
-        element.value = "";
-        toastr.error("Email must be specified");
+        toastr.error("Email must be between 8 to 50 characters");
     }
     else if (!emailRegex.test(element)) {
-        //errorMessages.push("Email is in Invalid Format");
-        element.value = "";
-        toastr.error("Email must be specified");
+        toastr.error("Email is in Invalid Format");
     }
 }
 
@@ -20,10 +15,7 @@
 
 function PasswordValidation(element) {
     if (element.value.length < 8 || element.value.length > 25) {
-        //errorMessages.push("Password should be between 8 to 25 characters");
         toastr.error("password must be specified");
-        element.value = "";
-
     }
 }
 
@@ -32,8 +24,6 @@ function PasswordValidation(element) {
 function NameValidation(element) {
     if (element.value.length < 2 || element.value.length > 100) {
         var attributeName = element.getAttribute("name");
-        //errorMessages.push("Password should be between 8 to 25 characters");
-        //toastr.error("Incorrect " + attributeName + " length");
         element.value = "";
         toastr.error("Should be between 2 and 100 characters");
     }
@@ -46,26 +36,10 @@ function NationalIdValidation(element) {
     var maxLength = 15;
     var NIDRegexCharacter = /^[a-zA-Z0-9]*$/;
     if (element.value.length < minLength || element.value.length > maxLength) {
-
-
-
-        //errorMessages.push("Incorrect NationalID length");
-
-
-
-
-
         toastr.error("National Id should be between 9 and 15 characters");
-        element.value = "";
     }
     if (!NIDRegexCharacter.test(element.value)) {
-        //errorMessages.push("Incorrect NationalID length");
-
-
-
         toastr.error("National Id should be between 9 and 15 characters");
-
-        element.value = "";
     }
 }
 
@@ -73,8 +47,6 @@ function NationalIdValidation(element) {
 
 function AddressValidation(element) {
     if ((element.value.length) < 2 || element.value.length > 30) {
-        // errorMessages.push("should be between 2 and 30 characters");
         toastr.error("should be between 2 and 15 characters");
-        element.value = "";
     }
 }
